df642790de7ffe032f4a7c32fa669013d375e7a1
Sam Chun <sanghun89@gmail.com>
2017-11-21 02:33:38 -0600

    Proposal draft

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,14 @@
 /* global document */
 
 import 'sass/styles.scss';
+import SliderModel from 'js/models/Slider';
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Example from 'js/components/Example';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import Example from 'js/components/Example';
+//
+// ReactDOM.render(<Example/>, document.getElementById('root'));
 
-ReactDOM.render(<Example/>, document.getElementById('root'));
+const slider = new SliderModel({
+	max: 2
+});
